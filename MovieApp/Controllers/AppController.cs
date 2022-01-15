3fe4edcfe1dee89770d3f49ce2923b848de2c600
CoderNuby <correofake12a@gmail.com>
2022-01-15 07:33:36 -0700

    Agregar archivos de proyecto.

---
+++ MovieApp/Controllers/AppController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieApp.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            throw new InvalidOperationException();
+            return View();
+        }
+
+        [HttpGet("acercaDe")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
