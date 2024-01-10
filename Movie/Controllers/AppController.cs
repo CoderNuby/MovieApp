abe61497250ab43ae03380080acbeda8bf191a4c
Luis Armando <asdf32easd@gmail.com>
2024-01-09 20:15:52 -0700

    aplicacion funcionando

--- Movie/Controllers/AppController.cs
+++ Movie/Controllers/AppController.cs
@@ -1,24 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Movie.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Movie.Controllers
 {
     public class AppController : Controller
     {
+        private static List<MovieViewModel> Movies = new List<MovieViewModel>();
+
         public IActionResult Index()
         {
-            return View();
+            return View(Movies);
         }
 
-        public IActionResult Edit()
+        public IActionResult AddOrEdit(Guid id)
         {
-            return View();
+            var movie = Movies.FirstOrDefault(x => x.Id == id);
+            if (movie == null)
+            {
+                return View();
+            }
+            return View(movie);
         }
 
         [HttpPost]
-        public IActionResult Edit(MovieViewModel movieVM)
+        public IActionResult AddOrEdit(MovieViewModel movieVM)
         {
+            if (ModelState.IsValid)
+            {
+                var movie = Movies.FirstOrDefault(x => x.Id == movieVM.Id);
+                if (movie == null)
+                {
+                    movieVM.Id = Guid.NewGuid();
+                    Movies.Add(movieVM);
+                }
+                else
+                {
+                    movie.Name = movieVM.Name;
+                    movie.Genre = movieVM.Genre;
+                }
+                return RedirectToAction("Index");
+            }
+
             return View();
+        }
+
+        public IActionResult Delete(Guid id)
+        {
+            var movie = Movies.FirstOrDefault(x => x.Id == id);
+            Movies.Remove(movie);
+            return RedirectToAction("Index");
         }
 
         [HttpGet("About-Us")]
--- Movie/Models/ViewModels/MovieViewModel.cs
+++ Movie/Models/ViewModels/MovieViewModel.cs
@@ -1,8 +1,18 @@
-﻿namespace Movie.Models.ViewModels
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Movie.Models.ViewModels
 {
     public class MovieViewModel
     {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(100)]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Genre is required")]
+        [MaxLength(50)]
         public string Genre { get; set; }
     }
 }
